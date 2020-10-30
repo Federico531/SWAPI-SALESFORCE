48b928174ea514390025ebac79fdd8fffa7552a6
Federico531 <59978769+Federico531@users.noreply.github.com>
2020-10-30 15:10:17 -0300

    Update swapiController.js

--- swapiController.js
+++ swapiController.js
@@ -1,34 +1,30 @@
-public with sharing class SwapiController {
-  /*  @AuraEnabled
-    espublic static List<Expense__c> getExpens() {
-        // Perform isAccessible() checking first, then
-        return [SELECT Id, Name, Amount__c, Client__c, Date__c,
-                Reimbursed__c, CreatedDate
-                FROM Expense__c];
-    } */
-    @AuraEnabled
-    public static Contact saveExpense(Contact contact) {
-        // Perform isUpdateable() checking first, then
-        upsert contact;
-        return contact;
-    } 
-    @AuraEnabled
-    public static List<Contact> getCharacters() {
-        // Check to make sure all fields are accessible to this user
-        String[] fieldsToCheck = new String[] {
-            'Name', 'Height__c', 'Gender__c', 'Haircolor__c',
-                'Eyecolor__c', 'URL__c', 'Planet__c', 'Characternumber__c'
-                };
-                    Map<String,Schema.SObjectField> fieldDescribeTokens =
-                    Schema.SObjectType.Contact.fields.getMap();
-        for(String field : fieldsToCheck) {
-            if( ! fieldDescribeTokens.get(field).getDescribe().isAccessible()) {
-                throw new System.NoAccessException();
+({
+    // Load expenses from Salesforce
+    doInit: function(component, event, helper) {
+        // Create the action
+        let action = component.get("c.getContacts");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.contacts", response.getReturnValue());
+                
             }
-        }
-        // OK, they're cool, let 'em through
-        return [SELECT Name, Height__c, Gender__c, Haircolor__c,
-                Eyecolor__c, URL__c, Planet__c, Characternumber__c
-                FROM Contact];
-    }
-}
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+    },
+    
+    handleUpdateContact: function(component, event, helper) {
+        let updatedExp = event.getParam("contact");
+        helper.updateContact(component, updatedExp);
+    },
+    handleCreateContact: function(component, event, helper) {
+        let newContact = event.getParam("contact");
+        helper.createContact(component, newContact);
+        alert("Contacto guardado")
+    },
+})
