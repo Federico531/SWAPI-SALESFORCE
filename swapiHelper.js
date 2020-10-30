0d0fad49b333009caf5cfd4916e15ea25fba1452
Federico531 <59978769+Federico531@users.noreply.github.com>
2020-10-30 18:45:44 -0300

    Update swapiHelper.js

--- swapiHelper.js
+++ swapiHelper.js
@@ -1,69 +1,27 @@
 ({
     //VERSION REDUCIDA
-    saveExpense: function(component, expense, callback) {
-        let action = component.get("c.saveExpense");
+    saveContact: function(component, contact, callback) {
+        let action = component.get("c.saveContact");
         action.setParams({
-            "expense": expense
+            "contact": contact
         });
         if (callback) {
             action.setCallback(this, callback);
         }
         $A.enqueueAction(action);
     },
-    createExpense: function(component, expense) {
-        this.saveExpense(component, expense, function(response){
+    createContact: function(component, contact) {
+        this.saveContact(component, contact, function(response){
             let state = response.getState();
             if (state === "SUCCESS") {
-                let expenses = component.get("v.expenses");
-                expenses.push(response.getReturnValue());
-                component.set("v.expenses", expenses);
+                let contacts = component.get("v.contacts");
+                contacts.push(response.getReturnValue());
+                component.set("v.contacts", contacts);
             }
         });
     },
-    updateExpense: function(component, expense) {
-        this.saveExpense(component, expense);
+    updateContact: function(component, contact) {
+        this.saveContact(component, contact);
     },
-    
-    //VERSION LARGA
-    /*
-    createExpense: function(component, expense) {
-        let action = component.get("c.saveExpense");
-        action.setParams({
-            "expense": expense
-        });
-        action.setCallback(this, function(response){
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                let expenses = component.get("v.expenses");
-                expenses.push(response.getReturnValue());
-                component.set("v.expenses", expenses);
-            }
-        });
-        $A.enqueueAction(action);
-    },
-    
-  	    createExpense: function(component, expense) {
-        let theExpenses = component.get("v.expenses");
-        // Copy the expense to a new object
-        // THIS IS A DISGUSTING, TEMPORARY HACK
-        let newExpense = JSON.parse(JSON.stringify(expense));
-        console.log("Expenses before 'create': " + JSON.stringify(theExpenses));
-        theExpenses.push(newExpense);
-        component.set("v.expenses", theExpenses);
-        console.log("Expenses after 'create': " + JSON.stringify(theExpenses));   
-        },
-      updateExpense: function(component, expense) {
-        let action = component.get("c.saveExpense");
-        action.setParams({
-            "expense": expense
-        });
-        action.setCallback(this, function(response){
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                // do nothing!
-            }
-        });
-        $A.enqueueAction(action);
-    },
-   */
+
 })
