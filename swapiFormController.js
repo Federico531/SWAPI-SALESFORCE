25bbd7c38eb8b99d314bd5203d1ca6c6a42e19b4
Federico531 <59978769+Federico531@users.noreply.github.com>
2020-10-30 18:59:00 -0300

    Update swapiFormController.js

--- swapiFormController.js
+++ swapiFormController.js
@@ -1,30 +1,18 @@
 ({
-    // Load expenses from Salesforce
-    doInit: function(component, event, helper) {
-        // Create the action
-        let action = component.get("c.getContacts");
-        // Add callback behavior for when response is received
-        action.setCallback(this, function(response) {
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.contacts", response.getReturnValue());
-                
-            }
-            else {
-                console.log("Failed with state: " + state);
-            }
-        });
-        // Send action off to be executed
-        $A.enqueueAction(action);
-    },
-    
-    handleUpdateContact: function(component, event, helper) {
-        let updatedExp = event.getParam("contact");
-        helper.updateContact(component, updatedExp);
-    },
-    handleCreateContact: function(component, event, helper) {
-        let newContact = event.getParam("contact");
-        helper.createContact(component, newContact);
-        alert("Contacto guardado")
-    },
+ clickCreate: function(component, event, helper) {
+
+        //component.find('aura:id')
+        let validContact = component.find('contactform').reduce(function (validSoFar, inputCmp) {
+            // Displays error messages for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        // If we pass error checking, do some real work
+        if(validContact){
+            // Create the new contact
+            let newContact = component.get("v.newContact");
+            console.log("Create Character: " + JSON.stringify(newContact));
+            helper.createContact(component, newContact);
+        }
+    }
 })
